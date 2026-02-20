68a236307ca2c01b981e10de6ac4f8b26126a1cb
thsp1900 <thsp1900@student.miun.se>
2026-02-20 09:48:29 +0100

    Add clone method

--- src/transtructiver/prototype/node.py
+++ src/transtructiver/prototype/node.py
@@ -51,6 +51,19 @@ class Node:
         for child in self.children:
             yield from child.traverse()
 
+    def clone(self):
+        """
+        Creates a deep copy of the current node and all its children.
+    
+        Returns:
+            Node: A new instance of Node with identical type, text, and 
+                recursively cloned children.
+        """
+        new_node = Node(self.type, text=self.text)
+        new_node.children = [child.clone() for child in self.children]
+
+        return new_node
+
     def __repr__(self):
         """Return a string representation of the node.
         
